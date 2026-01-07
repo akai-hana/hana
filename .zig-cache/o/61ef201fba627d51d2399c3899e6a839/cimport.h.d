8ae5f698f9d84dab29b3c5c78ecc24f108ddda87
akai-hana <akaihanatsundeee@proton.me>
2026-01-07 04:41:50 +0100

    chore: refresh cache to apply .gitignore

--- .zig-cache/o/61ef201fba627d51d2399c3899e6a839/cimport.h.d
+++
@@ -1,9 +0,0 @@
-cimport.o: \
-  /home/akai/git/hana/.zig-cache/o/61ef201fba627d51d2399c3899e6a839/cimport.h \
-  /usr/include/xcb/xcb.h /usr/include/sys/types.h \
-  /usr/include/features.h /usr/include/bits/alltypes.h \
-  /usr/include/endian.h /usr/include/sys/select.h \
-  /home/akai/zig-linux-x86_64-0.13.0/lib/include/stdint.h \
-  /usr/include/stdint.h /usr/include/bits/stdint.h \
-  /usr/include/sys/uio.h /usr/include/pthread.h /usr/include/sched.h \
-  /usr/include/time.h /usr/include/xcb/xproto.h
