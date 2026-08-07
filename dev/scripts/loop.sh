59fbb8c88bb99e886328e371e9d47a7bf60f066d
akai-hana <akaihanatsundeee@proton.me>
2026-08-08 01:44:17 +0200

    feat(dev): reduce commit batch size in loop script

--- dev/scripts/loop.sh
+++ dev/scripts/loop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-max=5
+max=1
+
 while true; do
     yes y | git-rewrite-commits --provider ollama --model hf.co/noctrex/Qwopus3.5-9B-Coder-MTP \
         --template "feat\(scope\): message" \
         --max-commits $max
 
     git push --force-with-lease
-    max+=5
+    max+=1
 done
