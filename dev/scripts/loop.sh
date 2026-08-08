9be14ff9e0c5dadcdef700762a73effeed6a60a9
akai-hana <akaihanatsundeee@proton.me>
2026-08-08 03:23:17 +0200

    feat(loop): adjust loop.sh to ensure script runs safely.

--- dev/scripts/loop.sh
+++ dev/scripts/loop.sh
@@ -3,7 +3,7 @@
 max=250
 
 while true; do
-    yes y | git-rewrite-commits --provider ollama --model hf.co/KevinJK51/Qwen3.6-12B-IQ-Ultra-Heretic-Uncensored-Thinking-V2-Hightop-GGUF:Q6_K \
+    yes y | git-rewrite-commits --provider ollama --model hf.co/KevinJK51/Qwen3.6-12B-IQ-Ultra-Heretic-Uncensored-Thinking-V2-Hightop-GGUF:IQ4_NL \
         --template "feat\(scope\): message" \
         --max-commits $max
 
